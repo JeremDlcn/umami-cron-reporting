8a59ace7c8bc0f2451d367225a183e9c93260e16
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-02-15 00:56:24 +0100

    🚧 Create PDF from umami

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
+const ImagesToPDF = require('images-pdf');
+
+
 
 (async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: true,
+        ignoreHTTPSErrors: true
+    });
     const page = await browser.newPage();
     await page.setViewport({
-        width: 640,
-        height: 480
-    })
+        width:1060,
+        height:745
+      })
     await page.goto(process.env.URL);
 
 
@@ -15,9 +21,13 @@ require('dotenv').config();
     await page.type('input[name=username]', process.env.USER);
     await page.type('input[name=password]', process.env.PASSWORD);
     await page.click('button[type=submit]');
-    await page.waitForNavigation();
+    await page.waitForNavigation({waitUntil: 'networkidle2'});
+
+    // Take page reporting
+    await page.screenshot({ path: 'images/umami-export.png', fullPage: true})
+    new ImagesToPDF.ImagesToPDF().convertFolderToPDF('images', 'output/umami.pdf');
 
 
-    await page.pdf({ path: 'umami-export.pdf', format: 'a4' });
+
     await browser.close();
 })();
