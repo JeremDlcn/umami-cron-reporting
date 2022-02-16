84f2c4b7d26476bb58abef48d8839639a8495d25
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-02-16 23:41:28 +0100

    🚧 try split pdf result

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 const ImagesToPDF = require('images-pdf');
+import { createPDF, splitPDF } from "pdf-actions";
+import JSZip from "jszip";
 
 (async () => {
     const browser = await puppeteer.launch({
@@ -29,14 +31,14 @@ const ImagesToPDF = require('images-pdf');
     // Capture all websites
     await page.waitForNavigation();
     await page.waitForSelector('div.Table_body__1_740')
-    const data = await page.evaluate(()=> {
+    const data = await page.evaluate(() => {
         let final = [];
         document.querySelectorAll('div.Table_body__1_740 .row a').forEach(elt => {
             final.push(elt.href)
         })
         return final
     })
-    for(const [i, url] of data.entries()){
+    for (const [i, url] of data.entries()) {
         await page.goto(url);
         await page.waitForSelector('.rsm-geographies')
         await page.waitForTimeout(1000)
@@ -44,6 +46,20 @@ const ImagesToPDF = require('images-pdf');
         console.log(`Screenshots taken ✨`);
     }
     new ImagesToPDF.ImagesToPDF().convertFolderToPDF('images', `output/umami.pdf`);
-    
+
+    const splitPDFHandler = async (files) => {
+        const zip = new JSZip();
+        for (let i = 0; i < files.length; i++) {
+            const file = files[i];
+            const pdfFile = await createPDF.PDFDocumentFromFile(file);
+            const splitPDF = await splitPDF(pdfFile, file.range, {degree: file.degrees})
+            const pdfFile = await splitPDF.save();
+                zip.file(`split-${file.name}`, pdfFile);
+        }
+        await zip.generateAsync({ type: "blob" }).then(function (content) {
+            saveAs(content, "splitPDFFiles.zip");
+        });
+    };
+
     await browser.close();
 })();
