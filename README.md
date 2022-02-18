d9b605c220acfdf8257f4d93520ca81b19fda2ec
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-02-18 19:05:21 +0100

    🚧 print Month on PDF

---
+++ README.md
@@ -0,0 +1,18 @@
+# Module to export data from Umami into PDFs
+
+## Installation
+create a .env file that contains:
+- URL: url of the website
+- USER: the user for the login
+- PASSWORD: the password for the login
+
+then install dependancies with npm
+```
+npm i
+```
+
+## Use
+finally you can launch the script manually or by a cron task with the command:
+```
+node script.js
+```
--- script.js
+++ script.js
@@ -22,15 +22,27 @@ if (!fs.existsSync('files')) fs.mkdirSync(`files`);
     await page.click('a[href*="settings"]')
     // Capture all websites
     await page.waitForNavigation();
+    //If an update is displayed hide it
+    if (await page.$('.UpdateNotice_notice__g5FIn button:last-of-type .Button_label___Frc25')!== null) await page.click('.UpdateNotice_notice__g5FIn button:last-of-type .Button_label___Frc25');
+
     await page.waitForSelector('div.Table_body__1_740')
     const data = await page.evaluate(() => {
         let final = [];
         document.querySelectorAll('div.Table_body__1_740 .row a').forEach(elt => {final.push({name: elt.textContent ,url:elt.href})})
         return final
     })
+    
+    
     for (const [i, site] of data.entries()) {
         await page.goto(site.url);
-        await page.waitForSelector('.rsm-geographies')
+        
+        //Select the month view
+        await page.waitForSelector('.Dropdown_dropdown__dINcM');
+        await page.click('.Dropdown_dropdown__dINcM');
+        await page.waitForSelector('.Menu_option__15tob:nth-of-type(5)');
+        await page.click('.Menu_option__15tob:nth-of-type(5)');
+        //Capture the page
+        await page.waitForSelector('.rsm-geographies');
         await page.waitForTimeout(1000)
         if (!fs.existsSync(`files/${site.name}`)) fs.mkdirSync(`files/${site.name}`);
         await page.screenshot({ path: `files/${site.name}/umami-export.png`, fullPage: true })
