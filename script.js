5bd2063f0436afa8e04539820b0b3b4781d6655e
Jerem Dlcn <44464165+JeremDlcn@users.noreply.github.com>
2022-02-26 19:57:48 +0100

    ☄ Sending all reportings by email

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ const path = require('path');
 //Check if the folder "files" exist
 if (!fs.existsSync('files')) fs.mkdirSync(`files`);
 
+//vars for email
+const dateForContent = new Date();
+const message = {
+    from: process.env.SENDER_EMAIL,
+    to: process.env.RECEIVER_EMAIL,
+    subject: 'Monthly Reporting ' + dateForContent.toLocaleString('default', { month: 'long', year: 'numeric' }).replace(/^\w/, c => c.toUpperCase()),
+    text: 'Reportings are in attachments',
+    attachments: []
+}
+
+const transporter = nodemailer.createTransport({ 
+    host: 'smtp-mail.outlook.com', secureConnection: false, port: 587, tls:{ ciphers: 'SSLv3' },
+    auth:{ user: process.env.SENDER_EMAIL, pass: process.env.SENDER_PASS }
+});
+
+
 (async () => {
     const browser = await puppeteer.launch({headless: false, ignoreHTTPSErrors: true});
     const page = await browser.newPage();
@@ -51,43 +67,25 @@ if (!fs.existsSync('files')) fs.mkdirSync(`files`);
         if (!fs.existsSync(`files/${site.name}`)) fs.mkdirSync(`files/${site.name}`);
         await page.screenshot({ path: `files/${site.name}/umami-export.png`, fullPage: true })
         new ImagesToPDF.ImagesToPDF().convertFolderToPDF(`files/${site.name}`, `files/Reporting-${site.name}.pdf`);
+
+        message.attachments.push({
+            filename: `Reporting-${site.name}.pdf`,
+            path: path.resolve(`files/Reporting-${site.name}.pdf`),
+            contentType: 'application/pdf'
+        })
     }
 
+
+    transporter.sendMail(message)
+    transporter.close();
+
+    
     await browser.close();
 })();
 
-//vars for email
-let fullPath = path.resolve('./files/Reporting.pdf');
-const dateForContent = new Date();
 
-// Email
-let transporter = nodemailer.createTransport(
-    {
-        host: 'smtp-mail.outlook.com',
-        secureConnection: false,
-        port: 587,
-        tls:{
-            ciphers: 'SSLv3'
-        },
-        auth:{
-            user: process.env.SENDER_EMAIL,
-            pass: process.env.SENDER_PASS
-        }
-    });
 
-let message = {
-    from: process.env.SENDER_EMAIL,
-    to: process.env.RECEIVER_EMAIL,
-    subject: 'Monthly Reporting ' + dateForContent.toLocaleString('default', { month: 'long', year: 'numeric' }).replace(/^\w/, c => c.toUpperCase()),
-    text: 'Reportings in attachments',
-    attachments: [
-        {
-            filename: 'Reporting.pdf',
-            path: fullPath,
-            contentType: 'application/pdf'
-        }
-    ]
-}
 
-transporter.sendMail(message)
-transporter.close();
+
+
+
